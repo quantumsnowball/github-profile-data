6999b356e0c292574dcb65871f5a93c1f96b982d
Quantum Snowball <quantum.snowball@gmail.com>
2022-09-07 21:06:59 +0800

    content: add github.io

--- WebDev.ts
+++ WebDev.ts
@@ -16,6 +16,11 @@ Web dev is easy but also can be difficult. It is easy when you have a boilerplat
   
   This project is still under development. When finished, it should be a ready-to-use boilerplate for a starter login app using the MERN stack.
 
+- ### [github.io](https://github.com/quantumsnowball/quantumsnowball.github.io) - github user page repo, holding the source code of the current website
+  ![typescript](https://img.shields.io/badge/typescript-white?logo=typescript) ![react](https://img.shields.io/badge/react-white?logo=react) ![redux](https://img.shields.io/badge/redux-white?logo=redux&logoColor=black) ![tailwindcss](https://img.shields.io/badge/tailwindcss-white?logo=tailwindcss)
+  
+  This project is the source code for my github user page. The production version is currently hosting at [quantumsnowball.github.io](https://quantumsnowball.github.io). The content of this website is based on the git submodule [github-profile-data](https://github.com/quantumsnowball/github-profile-data), which is also the content generator of my github profile page.
+
 - ### [js_todo_list_app](https://github.com/quantumsnowball/js_todo_list_app) - a pure javascript todos starter
   ![css](https://img.shields.io/badge/html5-white?logo=html5) ![javascript](https://img.shields.io/badge/javascript-white?logo=javascript) ![css](https://img.shields.io/badge/css-white?logo=css3&logoColor=blue)
 
